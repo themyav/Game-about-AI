e9a9f98eabe940f65a7d806c3426b4399b882ac3
myav <Myavochka119@gmail.com>
2020-07-16 19:39:47 +0700

    Добавила раздел "Info" и файл со шрифтами

---
+++ fonts.py
@@ -0,0 +1,5 @@
+#there will be more fonts here
+CENTURYGOTHIC = 'centurygothic'
+CONSOLAS = 'consolas'
+GEORGIA = 'georgia'
+IMPACT = 'impact'
--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 import pygame
 import colors as cl
+import fonts as ft
+#import show_info
 
 
 FPS = 60
@@ -33,8 +35,24 @@ class Button():
                 action()
         else:
             pygame.draw.rect(win, self.inactive_color, (x, y, self.w, self.h))
-        print_text(message, x + 39, y + 7)
+        print_text(message, x + self.w // 3 - 5, y + 7)
 
+def show_info():
+    info_win = pygame.image.load('info_back.png')
+    open_info = True
+    while open_info:
+        for i in pygame.event.get():
+            if i.type == pygame.QUIT:
+                return
+        win.blit(info_win, (0, 0))
+        info_file = open('info.txt')
+        info_text = info_file.readlines()
+        inf_y = 50
+        for i in info_text:
+            print_text(i, 50, inf_y, cl.GHOSTWHITE, ft.CONSOLAS, 20, True)
+            inf_y += 20
+            #нужно избавиться от переводов строк
+        pygame.display.update()
 
 def show_menu():
     global FPS
@@ -42,6 +60,7 @@ def show_menu():
     show = True
     start_button = Button(150, 50, cl.DEEPSKYBLUE, cl.POWDERBLUE)
     quit_button = Button(150, 50, cl.DEEPSKYBLUE, cl.POWDERBLUE)
+    info_button = Button(150, 50, cl.DEEPSKYBLUE, cl.POWDERBLUE)
     while show:
         for i in pygame.event.get():
             if i.type == pygame.QUIT:
@@ -49,13 +68,15 @@ def show_menu():
                 quit()
 
         win.blit(menu_background, (0, 0))
-        start_button.draw(W // 4 - 75, 380, 'Start', start_game)
-        quit_button.draw(W // 4 * 3 - 75, 380, 'Quit', quit)
+        start_button.draw(W // 3 - 75, 380, 'Start', start_game)
+        quit_button.draw(W // 3 * 2 - 75, 380, 'Quit', quit)
+        info_button.draw(W // 2 - 75, 480, 'Info', show_info)
+        print_text('Game about AI', W // 2 - 200, 150, cl.POWDERBLUE, 'centurygothic', 50, True)
         pygame.display.update()
     #clock.tick(FPS)
 
-def print_text(message, x, y, font_color = cl.BLACK, font_type = 'centurygothic', font_size = 30):
-    main_font = pygame.font.SysFont(font_type, font_size)
+def print_text(message, x, y, font_color = cl.BLACK, font_type = 'centurygothic', font_size = 30, is_bold = False, is_italic = False):
+    main_font = pygame.font.SysFont(font_type, font_size, is_bold, is_italic)
     text = main_font.render(message, True, font_color)
     win.blit(text, (x, y))
 
