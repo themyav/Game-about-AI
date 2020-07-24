1747dde77c9107c5d009ad687004df4081389e9c
ChechevatovR <chechevatov.r@gmail.com>
2020-07-24 13:34:32 +0700

    Design adjustments, template on all games

--- games/chinese_room/room.js
+++ games/chinese_room/room.js
@@ -26,6 +26,7 @@ function button() {
 }
 
 function check() {
+    // FIXME
     let input = document.getElementById("input_word").textContent;
     let output_got = document.getElementById("output_got").value;
     let output_correct = input;
@@ -88,34 +89,4 @@ function addRule() {
     docRules.append(newRule);
 
     rules_left_on_cur_length -= 1;
-}
-
-function change() {
-    let word = document.getElementById("new_word").textContent;
-    let user_answer = document.getElementById("answer").value;
-    let answer = '';
-    for (let i = 0; i < word.length; i++) {
-        answer += rules.get(word[i]);
-        if (word[i] === '<') i = i + 2;
-    }
-    document.getElementById("check").innerText = 'Проверить!';
-    if (i === 0) document.getElementById("result").innerHTML = '0/' + String(words_num) + ' верных ответов';
-    if (i > 0) {
-        let pos = "level" + String(i - 1);
-        if (answer === user_answer) {
-            document.getElementById(pos).innerHTML = 'Верно';
-            true_answers++;
-            document.getElementById("result").innerHTML = String(true_answers) + '/' + String(words_num) + ' верных ответов';
-        } else document.getElementById(pos).innerHTML = 'Ошибка';
-    }
-    if (i < words_num) {
-        document.getElementById("new_word").innerHTML = foreign_words[i];
-        pos = "rule" + String(j);
-        document.getElementById(pos).innerHTML = rules[j];
-    } else if (i == words_num) {
-        document.getElementById("new_word").innerHTML = 'Жми на кнопку "Проверить", чтобы сыграть еще раз.';
-        document.getElementById("check").innerText = 'Играть еще раз!';
-    }
-    i = (i + 1) % (words_num + 1);
-    j = (j + 1) % rules_num;
 }
