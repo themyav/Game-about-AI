cd8d6f578600aa6ce627c3fda03b1e80b40687ab
myav <Myavochka119@gmail.com>
2020-07-24 18:49:07 +0700

    Промежуточный commit: добавила класс весов, сделала игнорирование лишних клавиш.

--- games/control_the_net/control.js
+++ games/control_the_net/control.js
@@ -26,10 +26,36 @@ class Node{
         this.n_x = n_x;
         this.n_y = n_y;
     }
+    set_Weight(weight = 10){
+        this.weight = String(weight);
+    }
+    get_Weight(){
+        return self.weight;
+    }
     draw_Node(){
         context.drawImage(node, this.n_x, this.n_y);
+        context.font = "30px arial";
+        context.fillText(this.weight, this.n_x + 35, this.n_y + 65); //здесь нужно сделать выравнивание
     }
 
+}
+
+class Information{
+    constructor(inf_x, inf_y){
+        this.inf_x = inf_x;
+        this.inf_y = inf_y;
+    }
+    set_Weight(weight = 9){
+        this.weight = String(weight);
+    }
+    get_Weight(){
+        return this.weight;
+    }
+    draw_Information(){
+        context.drawImage(information, this.inf_x, this.inf_y);
+        context.font = "30px arial";
+        context.fillText(this.weight, this.inf_x + 20, this.inf_y + 40);
+    }
 }
 
 
@@ -45,10 +71,13 @@ let timer;
 function updateCanvas() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.drawImage(gameBackground, 0, 0);
-    context.drawImage(information, x, y);
+    let information = new Information(x, y);
+    information.set_Weight();
+    information.draw_Information();
     let node, new_x = 250, new_y = 140;
     for(let i = 0; i < 5; i++){
         node = new Node(new_x, new_y);
+        node.set_Weight();
         node.draw_Node();
         new_x += 120;
         new_y += 120;
@@ -76,13 +105,19 @@ function drawBackground(startingX, startingY) {
 }
 
 function processKey(e){
+    let prev_dx = dx;
+    let prev_dy = dy;
     dx = 0;
     dy = 0;
 
     if(e.keyCode == 38) dy -= step; //up
-    if(e.keyCode == 40) dy += step; //down
-    if(e.keyCode == 37) dx -= step; //left
-    if(e.keyCode == 39) dx += step; // right
+    else if(e.keyCode == 40) dy += step; //down
+    else if(e.keyCode == 37) dx -= step; //left
+    else if(e.keyCode == 39) dx += step; // right
+    else {
+        dx = prev_dx;
+        dy = prev_dy;
+    }
 
 }
 
@@ -103,7 +138,6 @@ TODO
 2)класс ребер. Ребра генерируются между каждой парой вершин
 3)два объекта - вход и выход соответственно
 4)коллизию
-5)веса, привязанные к вершинам и к информации. Тоже отдельный класс
 6)Действия игрока. Однонаправленность, но ВЕТВЛЕНИЕ
 7) нормальные картинки
 
