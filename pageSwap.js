c54a12832b5d2d97db2b821e18ee9425d8c3b9ea
apellpro <exhthx@mail.ru>
2020-07-26 13:47:53 +0700

    Единный скрипт переключения страниц. Фикс кода главной страницы и кнопки "Узнать больше".

--- pageSwap.js
+++ pageSwap.js
@@ -1,15 +1,13 @@
 var isSwapped = false;
 
-function swap() {
+function swapPage() {
     if (!isSwapped) {
         document.getElementById('supportPage').style.display = "block";
         document.getElementById('mainPage').style.display = "none";
         isSwapped = true;
-        clearInterval(IntervalId);
     } else {
         document.getElementById('supportPage').style.display = "none";
         document.getElementById('mainPage').style.display = "block";
         isSwapped = false;
-        IntervalId = setInterval(drawFrame, 10);
     }
 }
