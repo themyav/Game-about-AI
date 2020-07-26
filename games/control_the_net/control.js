425aab25c65a5a713e083dedccf4ea502fd6f846
myav <Myavochka119@gmail.com>
2020-07-27 01:10:40 +0700

    Изменение ребер, цветов и логики движения.

--- games/control_the_net/control.js
+++ games/control_the_net/control.js
@@ -9,9 +9,11 @@ let x = 0;
 let y = 0;
 
 //скорость перемещения
-let dx = 0;
+let dx = 1;
 let dy = 0;
 const step = 1; //длина шага
+let game_started = true;
+let prev_dx = dx, prev_dy = dy;
 
 let gameBackground = new Image();
 let information = new Image();
@@ -20,14 +22,18 @@ gameBackground.src = "resources/bc.png";
 information.src = "resources/new_info.png";
 node.src = "resources/node.png";
 
+//массивы
+
 let nodes = [];
 let edges = [];
+let turn = [];
+let colors = ["steelblue", "khaki", "coral"];
 
 function getRandom(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-function drawLine(cord, line_width, to_stroke, to_fill, color) { //coordinates
+function drawLine(cord, line_width, to_stroke, to_fill, color = "black") { //coordinates
     context.beginPath();
     let sx = cord[0], sy = cord[1];
     context.moveTo(sx, sy);
@@ -35,23 +41,32 @@ function drawLine(cord, line_width, to_stroke, to_fill, color) { //coordinates
         context.lineTo(cord[i], cord[i + 1]);
     }
     context.lineWidth = line_width;
-    if(to_stroke) context.stroke();
-    if(to_fill) context.fill();
+    if(to_stroke) {
+        context.strokeStyle = color;
+        context.stroke();
+    }
+    if(to_fill) {
+        context.fillStyle = color;
+        context.fill();
+    }
 
 }
 
 
 class Edge{
-    constructor(left, right) {
+    constructor(left, right, color) {
         self.left = left;
         self.right = right;
+        self.color = color;
     }
     draw_Edge(){
         let sx = self.left.x + node.width / 2;
         let sy = self.left.y + node.height / 2;
         let fx = self.right.x + node.width / 2;
         let fy = self.right.y + node.height / 2;
-        drawLine([sx, sy, fx, fy],  3, true, false);
+        drawLine([sx, sy, fx, sy],  3, true, false, self.color);
+        drawLine([fx, sy, fx, fy],  3, true, false, self.color);
+        turn.push({"x" : fx, "y" : sy});
     }
 }
 
@@ -69,6 +84,7 @@ class Node{
     draw_Node(){
         context.drawImage(node, this.n_x, this.n_y);
         context.font = "30px arial";
+        context.fillStyle = "black";
         context.fillText(this.weight, this.n_x + 35, this.n_y + 65); //здесь нужно сделать выравнивание
     }
 
@@ -96,7 +112,7 @@ class Information{
 window.onload = function () {
     canvas = document.getElementById("canvas");
     context = canvas.getContext("2d");
-    drawBackground(20, 20);
+    drawBackground(21.5, (80 + node.height / 2) - information.height / 2);
     window.onkeydown = processKey;
 };
 
@@ -104,10 +120,14 @@ function updateCanvas() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.drawImage(gameBackground, 0, 0);
 
-     drawLine([30, 80, 40, 20, 60, 20, 50, 80, 30, 80 ], 3, false, true);
+    context.font = "25px Arial";
+    context.fillText('Нажми на пробел, чтобы начать движение!', 680, 25);
+
+     drawLine([30, 80, 40, 20, 100, 20, 90, 80, 30, 80 ], 3, false, true, "darkred");
+      drawLine([1030, 880, 1040, 820, 1100, 820, 1090, 880, 1030, 880 ], 3, false, true, "darkred");
 
     for(let i = 0; i < edges.length; i++){
-        let edge = new Edge(edges[i].left, edges[i].right);
+        let edge = new Edge(edges[i].left, edges[i].right, edges[i].color);
         edge.draw_Edge();
     }
 
@@ -120,11 +140,9 @@ function updateCanvas() {
     let information = new Information(x, y);
     information.set_Weight();
     information.draw_Information();
-    checkCollision(x, y);
-
+    //дело в том, что желаемое перемещение должно быть разрешено реализовывать только в пределах нейрона, а не за ним.
 
 }
-
 
 function drawBackground(startingX, startingY) {
 
@@ -147,7 +165,7 @@ function drawBackground(startingX, startingY) {
             if(j == 0) node_x = node_x + getRandom(free);
             else node_x = node_x + 200 + getRandom(free);
             let node_weight = (3 + getRandom(20));
-            nodes.push({"x" : node_x, "y" : node_y, "weight" : node_weight});
+            nodes.push({"x" : node_x, "y" : node_y, "weight" : node_weight, "edges" : []});
         }
         node_y += 170;
     }
@@ -155,7 +173,22 @@ function drawBackground(startingX, startingY) {
     //генерация связей
     for(let i = 0; i < nodes.length; i++){
         for(let j = i + 1; j < nodes.length; j+=2){
-            edges.push({ "left" : nodes[i], "right" : nodes[j]});
+            let color = colors[getRandom(colors.length)];
+            let dir_x, dir_y;
+            let sx = nodes[i].x + node.width / 2;
+            let sy = nodes[i].y + node.height / 2;
+            let fx = nodes[j].x + node.width / 2;
+            let fy = nodes[j].y + node.height / 2;
+            if(sx == fx) dir_x = 0;
+            else if(sx < fx) dir_x = 1;
+            else dir_x = -1;
+            if(sy == fy) dir_y = 0;
+            else if(sy < fy) dir_y = 1;
+            else dir_y = -1;
+
+            edges.push({ "left" : nodes[i], "right" : nodes[j], "color" : color});
+            nodes[i].edges.push({ "left" : nodes[i], "right" : nodes[j], "dir_x" : dir_x, "dir_y" : dir_y});
+            nodes[j].edges.push({ "left" : nodes[i], "right" : nodes[j], "dir_x" : -dir_x, "dir_y" : -dir_y});
         }
     }
 
@@ -163,41 +196,46 @@ function drawBackground(startingX, startingY) {
 }
 
 function processKey(e){
-    let prev_dx = dx;
-    let prev_dy = dy;
-    dx = 0;
-    dy = 0;
 
-    if(e.keyCode === 38) dy -= step; //up
-    else if(e.keyCode === 40) dy += step; //down
-    else if(e.keyCode === 37) dx -= step; //left
-    else if(e.keyCode === 39) dx += step; // right
-    else {
-        dx = prev_dx;
-        dy = prev_dy;
-    }
-    let cx = x + dx, cy = y + dy;
-    checkCollision(cx, cy);
+        dx = 0;
+        dy = 0;
+
+        if(e.keyCode === 38) dy -= step; //up
+        else if(e.keyCode === 40) dy += step; //down
+        else if(e.keyCode === 37) dx -= step; //left
+        else if(e.keyCode === 39) dx += step; // right
+        else {
+            dx = prev_dx;
+            dy = prev_dy;
+        }
 }
 
 function checkCollision(cx, cy) {
     cx = cx + information.width / 2;
     cy = cy + information.height / 2;
-    for (let i = 0; i < nodes.length; i++){
+    for (let i = 0; i < nodes.length; i++) {
         let n_x = nodes[i].x + node.width / 2;
         let n_y = nodes[i].y + node.height / 2;
-        if( Math.sqrt((n_x - cx) * (n_x- cx)  + (n_y - cy) * (n_y- cy)) < information.width / 2 + node.width / 2){
-            dx = 0;
-            dy = 0;
-            break;
+        if (cx == n_x && cy == n_y) {
+            console.log(cx, cy, n_x, n_y);
+            return true;
         }
     }
+    return false;
 }
 
 function drawFrame() {
     if(dx != 0 || dy != 0){
-        x += dx;
-        y += dy;
+        if(checkCollision(x, y)){
+            x += dx;
+            y += dy;
+            prev_dx = dx;
+            prev_dy = dy; // меняем направление
+        }
+        else{
+            x += prev_dx;
+            y += prev_dy; //движемся как раньше
+        }
         updateCanvas();
     }
 }
@@ -216,15 +254,16 @@ function togglePause() {
 
     }
 }
-
 /*
 TODO
-1)Пересчет весов при столкновении.
-2)класс ребер. Ребра генерируются между каждой парой вершин
-(передается указатель на соответствующие вершины)
-Движение объекта не произвольно, а по ребру. Пока движешься по ребру, нельзя сменить направление.
-3)два объекта - вход и выход соответственно
-6)Действия игрока. Однонаправленность, но ВЕТВЛЕНИЕ
+1) Когда информация находится в центре нейрона:
+1.1 - проверка весов и алерт о проигрыше, если вес нейрона меньше.
+1.2 иначе:
+ - если dx == prev_x и dy == prev_y, то ищем первое свободное ребро и идем в него
+ - иначе проверяем, существует ли выбранное ребро. Если да, то идем в него, иначе в какое существует.
+2) Проверка поворотов
+- когда находимся в точке поворота изменяем направление
+3) Можно отдельную функцию взятия координат центра.
 7) нормальные картинки
 
 NOT TO DO!!!!!!!!!!!!!!!!!!!!!
