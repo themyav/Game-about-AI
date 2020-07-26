8f3d9494ccfe7f0c49cee97ad862eb199791a066
ChechevatovR <chechevatov.r@gmail.com>
2020-07-26 16:44:56 +0700

    visual changes in index page & exitScript changes for browser-mode support

--- exitScript.js
+++ exitScript.js
@@ -2,6 +2,6 @@ function exitPage() {
     let ans = confirm("Вы действительно хотите выйти их игры? Ваш прогресс будет утерян.");
 
     if (ans) {
-        document.location='/Game-about-AI';
+        document.location='../../';
     }
 }
