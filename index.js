8cd88e7a6d616aae369a7459e0c92faf4067f3a4
apellpro <exhthx@mail.ru>
2020-07-30 18:11:54 +0700

    Поправил анимацию

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ let menuIsHidden = true;
 function openInfo() {
     menuIsHidden = !menuIsHidden;
     if (!menuIsHidden) {
+        document.getElementById('hiddenInfo').classList.toggle('swap');
         document.getElementById('hiddenInfo').classList.toggle('openMenu');
         document.getElementById('textArrow').textContent = "▲";
     } else {
+        document.getElementById('hiddenInfo').classList.toggle('swap');
         document.getElementById('hiddenInfo').classList.toggle('openMenu');
         document.getElementById('textArrow').textContent = "Узнать больше ▼";
     }
